24c988bb3e5db8dba24a1ab7813c1ece20c31cff
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2020-05-13 13:25:25 +0100

    refactor lru to make it implement iCache

--- lru.go
+++ lru.go
@@ -9,7 +9,7 @@ func newLRU(size int) *lru {
 	}
 }
 
-// lru implements Cache
+// lru implements Cache and iCache interfaces
 // LRU evicts the least-recently used key.
 type lru struct {
 	size int
@@ -25,28 +25,71 @@ type lruNode struct {
 	previous *lruNode
 }
 
-func (c *lru) Read(key int) (int, bool) {
-	if _, exists := c.hash[key]; exists {
-		c.promote(key)
-		return c.hash[key].value, true
+// Cache interface
+
+func (c *lru) Read(key int) (value int, isCacheMiss bool) {
+	node := c.read(key)
+	if node == nil {
+		return 0, true
 	}
-	return 0, false
+	return node.(*lruNode).value, false
 }
 
 func (c *lru) Write(key, value int) {
-	if _, exists := c.hash[key]; exists {
-		c.hash[key].value = value
-		c.promote(key)
-		return
-	}
-	c.insert(key, value)
-	for c.isOverflowing() {
-		c.evictLast()
-	}
+	_, _ = c.write(key, value)
 }
 
+// iCache interface
+
+func (c *lru) read(key int) (interface{}) {
+	if node, exists := c.hash[key]; exists {
+		c.promote(key)
+		return node
+	}
+	return nil
+}
+
+func (c *lru) write(key, value int) (interface{}, interface{}) {
+	if node, exists := c.hash[key]; exists {
+		node.value = value
+		c.promote(key)
+		return node, nil
+	}
+	node := c.insert(key, value)
+	var evicted *lruNode
+	for c.isOverflowing() {
+		evicted = c.remove(c.last.key).(*lruNode)
+	}
+	return node, evicted
+}
+
+func (c *lru) remove(key int) (interface{}) {
+	node, found := c.hash[key]
+	if !found {
+		return nil
+	}
+	if node == c.head && node == c.last { // only one element in the cache
+		c.head = nil
+		c.last = nil
+	}
+	if node == c.head { // it's the head
+		node.next.previous = nil
+		c.head = node.next
+		node.next = nil
+	}
+	if node == c.last { // it's the last node
+		c.last = node.previous
+		node.previous.next = nil
+		node.previous = nil
+	}
+	delete(c.hash, node.key)
+	return node
+}
+
+// Helpers
+
 // insert assumes node does not yet exist in the hash table.
-func (c *lru) insert(key, value int) {
+func (c *lru) insert(key, value int) *lruNode {
 	newNode := &lruNode{
 		key:      key,
 		value:    value,
@@ -62,6 +105,7 @@ func (c *lru) insert(key, value int) {
 		c.head.previous = newNode
 		c.head = newNode
 	}
+	return newNode
 }
 
 func (c *lru) promote(key int) {
@@ -72,14 +116,16 @@ func (c *lru) promote(key int) {
 	if node.next == nil { // it's the last node
 		// detach from previous
 		node.previous.next = nil
+		c.last = node.previous
+		node.previous = nil
 		// move to the front of the head
 		c.head.previous = node
 		node.next = c.head
-		node.previous = nil
 		c.head = node
-		c.last = node
+		return
 	}
-	// detach from the head
+	// it's not last nor head.
+	// detach from previous and next
 	node.previous.next = node.next
 	node.next.previous = node.previous
 	// move to the front of the head
@@ -87,15 +133,8 @@ func (c *lru) promote(key int) {
 	node.next = c.head
 	c.head.previous = node
 	c.head = node
-
 }
 
 func (c *lru) isOverflowing() bool {
 	return len(c.hash) > c.size
-}
-
-func (c *lru) evictLast() {
-	delete(c.hash, c.last.key)
-	c.last.previous.next = nil
-	c.last = c.last.previous
 }
